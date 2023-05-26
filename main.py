52ed6b9bc3667b00ee9a8414f29009e722afaf62
theZoid9 <zaidakhalwaya6@gmail.com>
2023-05-26 13:57:33 +0200

    new functions

--- main.py
+++ main.py
@@ -1,29 +1,64 @@
 import random
+import time
 from pokemon_data import pokemon_names
+from pokemon_data import pokemon_n_only
 
 
+def bot_pokemon_choice():
+    
+    pokemon_list = list(pokemon_names.values())
+    pokemon_list = random.choice(pokemon_list)
+    bot = random.choice(pokemon_list)
+    return bot
+    
 
-def bot_choice():
+def player_pokemon_choice():
+    
+    player_input = input("choose Your Pokemon! \n")
+    for values in pokemon_names.values():
+        for x in values:
+            if x == player_input:
+                return player_input
+                
+        
+
+
+def player_pokemon_type():
+    pass
+
+
+def bot_pokemon_type():
     pass
 
 
 
-def player_choice():
-    player_input = input("chooseYour Pokemon!")
-    for values in pokemon_names.values():
-        for x in values:
-            if x == player_input:
-                     print(f"You choose{player_input}!!!")
-        
+def countdown():
+    
+     print("waiting for trainer...")
+     time.sleep(1)
+     print("waiting for trainer...")
+     time.sleep(1)
+     print("waiting for trainer...") 
+     time.sleep(1)
+     print("\n")
+ 
+
 
 
 
 def main():
-     pass
-
-
-
     
-player_choice()    
+    print(tuple(pokemon_n_only))
+    player_pokemon = player_pokemon_choice()
+    time.sleep(3)
+    print(f"lets Go !! {player_pokemon}!!! \n")
+    countdown()
+    bot_player = bot_pokemon_choice()
+    time.sleep(3)
+    print(f"Trainer player: I choose {bot_player} ..lets Fight!!\n")
+    time.sleep(5)
+    print(f"{player_pokemon} vs {bot_player}\n")
     
     
+    
+main()
